34a9c966e577bc7c2434be12785893464cbb313f
poorva-r <poorva.ramnani@gmail.com>
2024-04-16 16:30:10 +0400

    chatbot feature

--- fastapi_source/app/main.py
+++ fastapi_source/app/main.py
@@ -7,6 +7,9 @@ import pickle
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from fastapi import HTTPException
+from dotenv import load_dotenv
+import google.generativeai as ggi
+import markdown
 
 
 app = FastAPI()
@@ -18,7 +21,7 @@ templates = Jinja2Templates(directory="templates")
 models_folder = "models"
 model_path = os.path.join(models_folder, 'model.pkl')
 with open(model_path, 'rb') as model_file:
-    model = pickle.load(model_file)
+    ipc_model = pickle.load(model_file)
 
 encoder_path = os.path.join(models_folder, 'label_encoder.pkl')
 with open(encoder_path, 'rb') as encoder_file:
@@ -27,6 +30,22 @@ with open(encoder_path, 'rb') as encoder_file:
 vectorizer_path = os.path.join(models_folder, 'vectorizer.pkl')
 with open(vectorizer_path, 'rb') as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
+
+# Load environment variables from .env file
+load_dotenv(".env")
+
+# Fetch API key from environment variables
+fetched_api_key = os.getenv("API_KEY")
+print(fetched_api_key)
+
+# Configure the Gemini Pro API with the fetched API key
+ggi.configure(api_key=fetched_api_key)
+
+# Initialize the generative model (Gemini Pro)
+model = ggi.GenerativeModel("gemini-pro")
+
+# Start a chat session with the initialized model
+chat = model.start_chat()
 
 # Define landing page
 @app.get("/", response_class=HTMLResponse)
@@ -45,7 +64,7 @@ async def classify(request: Request, input_text: str = Form(...)):
     input_numerical = vectorizer.transform([input_text])
 
     # Make prediction using the trained model
-    prediction = model.predict(input_numerical.toarray())
+    prediction = ipc_model.predict(input_numerical.toarray())
 
     # Get top 3 predicted labels
     top_indices = np.argsort(prediction[0])[::-1][:3]
@@ -58,3 +77,26 @@ async def classify(request: Request, input_text: str = Form(...)):
 
     # Render the classifier page with the predictions
     return templates.TemplateResponse("classifier_page.html", {"request": request, "predictions": output})
+
+
+# Define chatbot page
+@app.get("/chatbot", response_class=HTMLResponse)
+async def chatbot_page(request: Request):
+    return templates.TemplateResponse("chatbot_page.html", {"request": request})
+
+# Define endpoint to handle chatbot messages
+@app.post("/chatbot-query")
+async def chatbot_message(request: Request, message: str = Form(...)):
+
+    try:
+        # Get response from the Gemini API for the user's question
+        response = chat.send_message(message, stream=True)
+
+        # Extract text from the response object and wrap lines
+        response_text = "\n".join([word.text for word in response])
+
+        # Return the response
+        return {"message": response_text}
+    except Exception as e:
+        return {"error": str(e)}
+
